709f63a3881c208e96657d4f2b0cd73d280688a9
Deezeddd <kalpved123@gmail.com>
2024-06-18 00:15:53 +0530

    update: CRUD APIs created

--- exploro-back/api.py
+++ exploro-back/api.py
@@ -28,11 +28,9 @@ def postTest():
     return jsonify(response.data)   
 
 
-@app.route("/post", methods=["GET"])
+@app.route("/post", methods=["POST"])
 def post():
-    if request.method != 'GET':
-        return "Error"
-    
+
     # Get data from URL parameters
     id = request.args.get('id', default=random.randint(1, 100), type=int)
     
@@ -48,35 +46,27 @@ def post():
     
     # Insert data into Supabase table
     response = supabase.table('yodo').insert(data).execute()
-
+    print(jsonify(response.data))
     # Return the response data as JSON
     return jsonify(response.data)
 
-@app.route("/put", methods=["GET"])
+@app.route("/put", methods=["PUT"])
 def put():
-    if request.method != 'GET':
-        return "Error"
-    
-    # Get data from URL parameters
-    id = request.args.get('id', default=random.randint(1, 100), type=int)
-    
-    name = request.args.get('name', default="Yodo " + str(id), type=str)
 
+    # Get data from URL parameters
+    id = request.args.get('id', type=int)
+    data = {"name": "PODA 00"}
     response = supabase.table('yodo').select('id').eq('id', id).execute()
     if response.data:
-        # ID already exists
-        return jsonify({"error": "ID already exists"}), 400
-
-    # Prepare data
-    data = {'name': name, 'id': id, "created_at": str(datetime.datetime.now())}
-    
-    # Insert data into Supabase table
-    response = supabase.table('yodo').insert(data).execute()
-
+        # ID Doesn't exists
+        response = supabase.table('yodo').update(data).eq('id', id).execute()
+    else: 
+        return jsonify({"error": "ID doesn't exists"}), 400
+   
     # Return the response data as JSON
-    return jsonify(response.data)
+    return jsonify(response.data, "Updated")    
 
-@app.route("/delete", methods=["GET"])
+@app.route("/delete", methods=["DELETE"])
 def delete():
 
     # Get data from URL parameters
